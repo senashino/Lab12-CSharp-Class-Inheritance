0eed4ae147ff130f81b7d96f32b2310126f311b8
senashino <phwn26@gmail.com>
2025-03-08 20:52:11 +0700

    update lab12-01-10

---
+++ Lab12_EX05/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+interface IBaseClass1
+{
+    void Method1();
+}
+
+interface IBaseClass2
+{
+    void Method2();
+}
+
+class BaseClass1 : IBaseClass1
+{
+    public BaseClass1()
+    {
+        System.Console.WriteLine("This is BaseClass1");
+    }
+    
+    public void Method1()
+    {
+        System.Console.WriteLine("Method1 from BaseClass1");
+    }
+}
+
+class BaseClass2 : IBaseClass2
+{
+    public BaseClass2()
+    {
+        System.Console.WriteLine("This is BaseClass2");
+    }
+    
+    public void Method2()
+    {
+        System.Console.WriteLine("Method2 from BaseClass2");
+    }
+}
+
+class DerivedClass : BaseClass1, IBaseClass2
+{
+    public DerivedClass() : base() // เรียกคอนสตรัคเตอร์ของ BaseClass1
+    {
+        System.Console.WriteLine("This is DerivedClass");
+    }
+    
+    public void Method2()
+    {
+        System.Console.WriteLine("Method2 from DerivedClass");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DerivedClass dc = new DerivedClass();  // สร้างอ็อบเจกต์ของ DerivedClass
+    }
+}
